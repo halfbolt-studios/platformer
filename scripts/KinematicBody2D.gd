725d752d5be99e3ceec86f39ef4c38317815048a
Ethan <coder.efe@gmail.com>
2019-04-08 09:39:07 -0700

    First commit to add project

---
+++ scripts/KinematicBody2D.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const SPEED = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Vector2()
+
+	if Input.is_action_pressed("ui_down"):
+        direction.y += SPEED
+	if Input.is_action_pressed("ui_up"):
+        direction.y += -SPEED
+	if Input.is_action_pressed("ui_left"):
+        direction.x += -SPEED
+	if Input.is_action_pressed("ui_right"):
+        direction.x += SPEED
+	#print("x: " + str(direction.x) + ", y: " + str(direction.y))
+	#set_pos(get_pos() + direction.normalized() * SPEED * delta)
+	direction = move_and_slide(direction)
+	pass
