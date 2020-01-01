08c0d00f3d6ad5704215b5fd8eb3a244a648959f
Neil Macneale <neil.macneale.v@gmail.com>
2020-01-01 00:18:37 -0800

    Made collidable object kinematic bodies, and gave them a feel of weight when you push them

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export var speed: int
-export var mass: int
+export var slow_down: float
 
 func _physics_process(delta_time):
   var delta = Vector2()
@@ -14,5 +14,15 @@ func _physics_process(delta_time):
     delta.x += -1
   if Input.is_action_pressed("move_right"):
     delta.x += 1
-  delta = delta.normalized() * speed
-  move_and_slide(delta)
+  delta = delta.normalized() * speed * delta_time
+  var collision = move_and_collide(delta)
+  if collision != null:
+    # reflect is a vector that will push you out of the wall and make you slide
+    var reflect = collision.remainder.bounce(collision.normal)
+    if collision.collider is TileMap:
+      move_and_collide(reflect)
+    if collision.collider is KinematicBody2D:
+      # make other body move in same direction at slower speed
+      collision.collider.move_and_collide((delta - delta.slide(collision.normal)) * slow_down)
+      # push player out of other object, but not as much, so that you push it along
+      move_and_collide(reflect * (1 - slow_down))
