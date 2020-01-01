f28e2b2eef338df1215e011391649c8751d4583b
Neil Macneale <neil.macneale.v@gmail.com>
2019-12-31 17:16:33 -0800

    Reformatted player code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,18 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-const SPEED = 200
+export var speed: int
+export var mass: int
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta_time):
+  var delta = Vector2()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var direction = Vector2()
-
-	if Input.is_action_pressed("ui_down"):
-        direction.y += SPEED
-	if Input.is_action_pressed("ui_up"):
-        direction.y += -SPEED
-	if Input.is_action_pressed("ui_left"):
-        direction.x += -SPEED
-	if Input.is_action_pressed("ui_right"):
-        direction.x += SPEED
-	#print("x: " + str(direction.x) + ", y: " + str(direction.y))
-	#set_pos(get_pos() + direction.normalized() * SPEED * delta)
-	#direction = move_and_slide(direction)
-	direction = move_and_slide(direction)
-	pass
-
-func get_pos():
-	return position
+  if Input.is_action_pressed("move_up"):
+    delta.y += -1
+  if Input.is_action_pressed("move_down"):
+    delta.y += 1
+  if Input.is_action_pressed("move_left"):
+    delta.x += -1
+  if Input.is_action_pressed("move_right"):
+    delta.x += 1
+  delta = delta.normalized() * speed * delta_time
+  move_and_slide(delta)
