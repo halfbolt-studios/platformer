04a1646a5e16377ea5b26cdff2254aa14074999b
Neil Macneale <neil.macneale.v@gmail.com>
2020-01-01 16:08:58 -0800

    Reformatted enemy script

--- scripts/camera.shader
+++ scripts/camera.shader
@@ -4,6 +4,6 @@ uniform float vignette_scale;
 
 void fragment() {
   vec3 col = texture(TEXTURE, UV).xyz;
-  float dist_to_center = length(SCREEN_UV - 0.5) * -2.0 + 1.0 * vignette_scale;
+  float dist_to_center = min(length(SCREEN_UV - 0.5) * -2.0 + 1.0 * vignette_scale, 1);
   COLOR.xyz = col * dist_to_center;
 }
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,54 +1,44 @@
 extends KinematicBody2D
-#extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var level = get_parent().get_node("Navigation2D/TileMap")
-var path = []
-const SPEED = 100
-var direction = Vector2(0,0)
+export var speed = 100
+export var debug = false
 
-"""
-	#very basic enemy AI, we need to use a better one
-	if position.y < player.get_pos().y:
-        direction.y += SPEED
-	if position.y > player.get_pos().y:
-        direction.y += -SPEED
-	if position.x > player.get_pos().x:
-        direction.x += -SPEED
-	if position.x < player.get_pos().x:
-        direction.x += SPEED
-"""
+var level
+var path
+var direction = Vector2()
+var player
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	set_process(true)
-	path = level.get_path_to_player(self)
-	pass # Replace with function body.
+  level = get_parent().get_node("Navigation2D/TileMap")
+  path = level.get_path_to_player(self)
+  player = get_parent().get_node("player")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	update()
-	var player = get_parent().get_node("player")
-	if (path.size() > 1):
-		var pos_to_move = path[path.size() - 2]
-		direction = (pos_to_move - position).normalized()
-		var distance = position.distance_to(path[path.size() - 2])
-		if (distance > 1):
-			#position += SPEED * delta * direction
-			move_and_slide(direction * SPEED)
-			#direction = move_and_slide(SPEED * delta * direction)
-		else:
-			path.remove(len(path) - 1)
-	else:
-		path = level.get_path_to_player(self)
-	#refresh path if player has moved from its previous tile
-	if (path.size() > 0 and player.position.distance_to(path[0]) > 17):
-		path = level.get_path_to_player(self)
-	pass
+  if debug:
+    update()
+  if (path.size() > 1):
+    var pos_to_move = path[path.size() - 2]
+    direction = (pos_to_move - position).normalized()
+    var distance = position.distance_to(path[path.size() - 2])
+    if (distance > 1):
+      #position += SPEED * delta * direction
+      move_and_slide(direction * speed)
+      #direction = move_and_slide(SPEED * delta * direction)
+    else:
+      path.remove(len(path) - 1)
+  else:
+    path = level.get_path_to_player(self)
+  #refresh path if player has moved from its previous tile
+  if (path.size() > 0 and player.position.distance_to(path[0]) > 17):
+    path = level.get_path_to_player(self)
 
-#draws enemy path
+# draws enemy path
 func _draw():
-	for index in range(len(path) - 1):
-		draw_line(Vector2(path[index].x - position.x, path[index].y - position.y), Vector2(path[index + 1].x - position.x, path[index + 1].y - position.y), Color(255, 0, 0), 1)
+  if !debug:
+    return
+  for i in range(len(path) - 1):
+    draw_line(Vector2(path[i].x - position.x,
+                      path[i].y - position.y),
+              Vector2(path[i + 1].x - position.x,
+                      path[i + 1].y - position.y),
+              Color(255, 0, 0), 1)
